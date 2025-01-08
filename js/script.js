3c320166f7b07f0340f8be657af9674849794589
Blake <btsmith1212@gmail.com>
2025-01-07 21:00:58 -0600

    added newsletter,footer,mobile menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 const tabs = document.querySelectorAll('.tab')
 const panels = document.querySelectorAll('.panel')
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+const logo = document.getElementById('logo')
+
 
 //tabs menu event listener
 tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
+
+//hamburger button listener
+
+btn.addEventListener('click', navToggle)
 
 function onTabClick(e) {
     //deactivate all tabs
@@ -23,4 +31,16 @@ function onTabClick(e) {
     document.getElementById('panels')
         .getElementsByClassName(classString)[0]
         .classList.remove('hidden')
+}
+
+function navToggle(){
+    btn.classList.toggle('open')
+    menu.classList.toggle('flex')
+    menu.classList.toggle('hidden')
+
+    if(menu.classList.contains('flex')){
+        logo.setAttribute('src', './images/logo-bookmark-footer.svg')
+    } else {
+        logo.setAttribute('src', './images/logo-bookmark.svg')
+    }
 }
